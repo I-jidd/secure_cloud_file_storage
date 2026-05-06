7211d9cb75b7f997c9178b89eb2f53985635e98e
I-jidd <jade.notsopersonal@gmail.com>
2026-05-06 16:50:06 +0800

    feat: create FastAPI application entrypoint

---
+++ backend/app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.core.config import settings
+
+
+app = FastAPI(
+    title= settings.APP_NAME,
+    version = "0.1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials =True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {
+        "message": "Welcome to Secure Cloud Storage API"
+    }
