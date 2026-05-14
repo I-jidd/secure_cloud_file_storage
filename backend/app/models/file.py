08938fa1f64656a2af9cbad3f883ac19adab6953
I-jidd <jade.notsopersonal@gmail.com>
2026-05-14 23:32:12 +0800

    feat: add File database model

---
+++ backend/app/models/file.py
@@ -0,0 +1,92 @@
+import uuid
+from datetime import datetime, timezone
+
+from sqlalchemy import BigInteger, Boolean, String, DateTime, ForeignKey, Text
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.db.database import Base
+
+def utc_now():
+    return datetime.now(timezone.utc)
+
+class File(Base):
+    __tablename__ = "files"
+    
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        index = True
+    )
+    
+    original_name: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+        index=True
+    )
+    
+    stored_name: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+        unique=True
+    )
+    
+    storage_path: Mapped[str] = mapped_column(
+        Text,
+        nullable=False
+    )
+    
+    mime_type: Mapped[str|None] = mapped_column(
+        String(100),
+        nullable=True,
+        index=True
+    )
+    
+    size_bytes: Mapped[int] = mapped_column(
+        BigInteger,
+        nullable=False
+    )
+    
+    owner_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+        index=True
+    )
+    
+    folder_id: Mapped[uuid.UUID | None] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey("folders.id", ondelete="SET NULL"),
+        nullable=True,
+        index=True
+    )
+    
+    is_deleted: Mapped[datetime | None] = mapped_column(
+        Boolean,
+        default=False,
+        nullable=False,
+        index=True
+    )
+    
+    deleted_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True),
+        nullable=True
+    )
+    
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=utc_now,
+        onupdate=utc_now,
+        nullable=False
+    )
+    
+    owner = relationship(
+        "User",
+        backref="files"
+    )
+    
+    folder = relationship(
+        "Folder",
+        backref="files"
+    )
