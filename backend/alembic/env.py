3e4aeecc439b3af02cc80447addbf6b82e54aad3
I-jidd <jade.notsopersonal@gmail.com>
2026-05-15 10:34:45 +0800

    chore: import File model into Alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -8,6 +8,7 @@ from alembic import context
 from app.db.database import Base
 from app.models.user import User
 from app.models.folder import Folder
+from app.models.file import File
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
