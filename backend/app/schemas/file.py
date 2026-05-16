a40448a65cfc8939019ca0a67969138cd05da628
I-jidd <jade.notsopersonal@gmail.com>
2026-05-16 23:47:01 +0800

    feat: add file Pydantic schemas

---
+++ backend/app/schemas/file.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+class FileUpdate(BaseModel):
+    original_name: str = Field(min_length=1, max_length=255)
+    
+
+class File_Response(BaseModel):
+    id: UUID
+    original_name: str
+    stored_name: str
+    storage_path:str
+    mime_type: str | None
+    size_bytes: int
+    owner_id: UUID
+    folder_id: UUID | None
+    is_deleted: bool
+    deleted_at: datetime | None
+    created_at: datetime
+    updated_at: datetime
+    
+    model_config = {
+        "from_attributes": True
+    }
